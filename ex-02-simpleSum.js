bdcbc9a7afac23c895120809c2cebe9b70bbd589
andicoaker <Andi@Andreas-MacBook.local>
2017-01-23 13:39:56 -0500

    initial files

---
+++ ex-02-simpleSum.js
@@ -0,0 +1,20 @@
+
+/**
+ * PART 02 : simpleSum()
+ *
+ * Write a function called sum that takes two numbers as
+ * inputs and computes the sum of those two numbers.
+ */
+
+function simpleSum(num1, num2){
+	//YOUR CODE HERE
+   //  -- also, you will need to write the functions for
+   //     remaining problems in the problem set
+   var result = num1 + num2;
+   return result;
+}
+
+
+console.assert( simpleSum(8, 11) === 19 );
+console.assert( simpleSum(4, 101) === 105 );
+console.assert( simpleSum(2, 2) !== 5 );
